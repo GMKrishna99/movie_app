c2f09f809b8956b546166638d8e7a7e5c26724d4
GMKrishna99 <gmohankrishna23@gmail.com>
2025-03-08 12:49:09 +0530

    removed unwanted files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-100">
       <Text className="text-5xl text-primary font-bold ">Hello world</Text>
-      <Link href={"/onboarding"}>Onboarding</Link>
-      <Link href={"/movie/avangers"}>Avenger Movie</Link>
     </View>
   );
 }
